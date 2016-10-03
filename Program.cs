4cf895c97978200d3fabeb036e87ab8bcc010d45
Sai Tejaswini Natani <saitejaswininatani@Sais-MacBook-Pro.local>
2016-10-03 16:27:14 -0700

    First

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string,string> profile = new Dictionary<string,string>();
+            //Almost all the methods that exists with Lists are the same with Dictionaries
+            profile.Add("Name", "Speros");
+            profile.Add("Language", "PHP");
+            profile.Add("Location", "Greece");
+            Console.WriteLine("Instructor Profile");
+            Console.WriteLine("Name - " + profile["Name"]);
+            Console.WriteLine("From - " + profile["Location"]);
+            Console.WriteLine("Favorite Language - " + profile["Language"]);
+            Console.WriteLine("**********");
+            foreach (KeyValuePair<string,string> entry in profile)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+            Console.WriteLine("**********");
+            ////Iterating in direct way/////////////////
+            //The var keyword takes the place of a type in type-inference
+            foreach (var entry in profile)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+        }
+    }
+}
